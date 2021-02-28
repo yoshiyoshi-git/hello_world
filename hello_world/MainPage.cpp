4d17caed248f96bc5af5074aa1682e998823235c
lilac <github@nijisanji.sakura.ne.jp>
2021-02-28 19:58:16 +0900

    プロジェクト ファイルを追加します。

---
+++ hello_world/MainPage.cpp
@@ -0,0 +1,57 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+
+//追加
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.UI.Popups.h>
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::hello_world::implementation
+{
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+        myButton().Content(box_value(L"Clicked"));
+    }
+}
+
+
+void winrt::hello_world::implementation::MainPage::button_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::RoutedEventArgs const& e)
+{
+    //追加
+    winrt::Windows::UI::Xaml::Controls::ContentDialog dialog;
+    dialog.Title(box_value(L"hello"));
+    dialog.Content(box_value(L"Hello World"));
+    dialog.CloseButtonText(L"OK");
+    dialog.PrimaryButtonText(L"OKOK");
+    dialog.ShowAsync();
+
+    Windows::UI::Popups::MessageDialog msg{ L"Hello World",L"Hello World" };
+    msg.ShowAsync();
+
+}
+
+
+void winrt::hello_world::implementation::MainPage::button1_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::RoutedEventArgs const& e)
+{
+    //追加
+    textBlock().Text(L"Hello World");
+
+}
---
+++ hello_world/MainPage.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::hello_world::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        void button_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::RoutedEventArgs const& e);
+        void button1_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::RoutedEventArgs const& e);
+    };
+}
+
+namespace winrt::hello_world::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
